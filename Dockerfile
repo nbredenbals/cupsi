be3a754cb12c09d5b7956796149780b178b3dd7c
Nico Bredenbals <nico@bredenbals.net>
2026-02-12 15:25:14 +0100

    Switch to own image

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+FROM debian:stable-slim
+LABEL org.opencontainers.image.authors="https://github.com/nbredenbals"
+
+VOLUME /etc/cups
+
+RUN apt-get update \
+&& apt-get install -y \
+  sudo \
+  gettext-base \
+  usbutils \
+  cups \
+  cups-bsd \
+  cups-filters \
+  cups-browsed \
+  foomatic-db-engine \
+  foomatic-db-compressed-ppds \
+  openprinting-ppds \
+  hp-ppd \
+  printer-driver-hpcups \
+  printer-driver-foo2zjs \
+  printer-driver-pnm2ppa \
+  printer-driver-cups-pdf \
+&& apt-get clean \
+&& rm -rf /var/lib/apt/lists/*
+
+RUN cp -R  /etc/cups /etc/cups_original
+
+# This will use port 631
+#EXPOSE 631
+
+RUN mkdir -p /opt/cupsi/
+COPY --chown=root:lp --chmod=644 cupsd.conf.tpl /opt/cupsi/cupsd.conf.tpl
+COPY --chown=root:lp --chmod=644 cups-files.conf /opt/cupsi/cups-files.conf
+
+COPY --chmod=700 entrypoint.sh /entrypoint.sh
+
+COPY --chown=root:root --chmod=644 nsswitch.conf /etc/nsswitch.conf
+
+# Remove default config files to force usage of our templated ones
+RUN rm /etc/cups/cupsd.conf \
+       /etc/cups/cups-files.conf
+
+# Run as root, as CUPS needs to bind to low ports and manage devices
+ENTRYPOINT ["/entrypoint.sh"]
+
+# Default shell
+CMD ["/usr/sbin/cupsd", "-f"]
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+if [ ! -f /etc/cups/cupsd.conf ]; then
+    echo "Generating /etc/cups/cupsd.conf from template"
+    LOGLEVEL="${LOGLEVEL:-warn}" \
+    LOCALNET="${LOCALNET:-192.168.1.*}" \
+    CUPS_HOSTNAME="${CUPS_HOSTNAME:-`hostname -f`}" \
+    CUPS_PORT="${CUPS_PORT:-443}" \
+    envsubst < /opt/cupsi/cupsd.conf.tpl > /etc/cups/cupsd.conf
+fi
+
+if [ ! -f /etc/cups/cups-files.conf ]; then
+    echo "Copying default /etc/cups/cups-files.conf"
+    cp /opt/cupsi/cups-files.conf /etc/cups/cups-files.conf
+fi
+
+
+CUPSUSER=${CUPSUSER:-cupsadm}
+CUPSPASSWORD="${CUPSPASSWORD:-`cat ${CUPSPASSWORD_FILE}`}"
+if [ -z $CUPSPASSWORD ]; then 
+    CUPSPASSWORD=$(openssl rand -base64 12)
+    echo "Generated random CUPS password for user ${CUPSUSER}: ${CUPSPASSWORD}" >&2
+fi
+
+mkdir -p /etc/cups/ssl
+
+# Add the user `cups` if it does not exist
+# The user does not run any processes, but is member in lp and lpadmin
+# to allow web access to printer administration
+if ! id "${CUPSUSER}" > /dev/null 2>&1; then
+    addgroup ${CUPSUSER}
+    useradd -G lp,lpadmin -g ${CUPSUSER} --shell /bin/false "${CUPSUSER}"
+    echo ${CUPSPASSWORD} | passwd ${CUPSUSER} --stdin >/dev/null 2>&1
+fi
+
+# Run the standard container command.
+exec "$@"
